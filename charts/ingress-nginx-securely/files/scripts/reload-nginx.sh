f81926203cdbcc3946e58f635d26f693a8e812e6
Jochem Bruijns <jochem.bruijns@cyso.com>
2023-12-06 15:28:35 +0100

    Add scripts to securely configuration, enable wait time for nginx to come online in pod

--- charts/ingress-nginx-securely/files/scripts/reload-nginx.sh
+++ charts/ingress-nginx-securely/files/scripts/reload-nginx.sh
@@ -1,5 +1,19 @@
 #/bin/sh
+while true
+do
+  pgrep "nginx: master" > /dev/null 2>&1
+  exit_status=$?
+  if [ $exit_status -eq 0 ]
+  then
+    echo "Nginx process found, continuing script"
+    break
+  else
+    echo "Nginx process not found, sleeping for 5 seconds"
+    sleep 5
+  fi
+done
+
 NGINX_PID=$(pgrep "nginx: master")
 
-echo "Reloading NGINX, PID: $PID"
+echo "Reloading NGINX, PID: $NGINX_PID"
 kill -HUP $NGINX_PID
