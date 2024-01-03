1ebf852c5ec03d5c44e921ddfb72346aa270f52b
Jochem Bruijns <jochem.bruijns@cyso.com>
2024-01-03 14:43:07 +0100

    Add additional blocker rule for IP DB and fix bug in configuration

--- charts/ingress-nginx-securely/files/scripts/init-files.sh
+++ charts/ingress-nginx-securely/files/scripts/init-files.sh
@@ -4,5 +4,6 @@ mkdir /opt/securely/conf
 
 # Set blocker modsec rule with IP database
 echo 'SecRule REQUEST_HEADERS:X-Forwarded-for "@ipMatchFromFile /opt/securely/blocker-db" "id:300000,phase:1,t:none,log,auditlog,msg:'IP blocked due to Securely IP Blocking Service',deny,ctl:ruleEngine=On"' > /opt/securely/conf/blocker.conf 
+echo 'SecRule REMOTE_ADDR "@ipMatchFromFile /opt/securely/blocker-db" "id:300100,phase:1,t:none,log,auditlog,msg:'IP blocked due to Securely IP Blocking Service',deny,ctl:ruleEngine=On"' >> /opt/securely/conf/blocker.conf 
 
 chmod -R 777 /opt/securely
